79674c2acd67587345068b696d36478e9342506f
aleksey <kolohok@yandex.ru>
2023-11-20 12:05:00 +0300

    Deleted scanner and add added features for checking the deletion of free

--- src/vsu/kurs2/Task.java
+++ src/vsu/kurs2/Task.java
@@ -8,13 +8,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Task {
-    public static void deletePolygons(Model model) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите номер полигона - ");
-        int k = scanner.nextInt();
+    public static void deletePolygons(Model model, int k, boolean delete) {
 
         model.polygons.remove(k);
-        scanner.close();
         boolean[] usedVertices = new boolean[model.vertices.size()];
         for (Polygon polygon : model.polygons) {
             ArrayList<Integer> vertexIndices = polygon.getVertexIndices();
@@ -24,15 +20,9 @@ public class Task {
                 }
             }
         }
-
         for (int i = usedVertices.length - 1; i >= 0; i--) {
-            if (!usedVertices[i]) {
-                System.out.println("Хотите ли вы удалить вершину под индексом " + i + "?");
-                if (scanner.nextLine().equals("Да")){
+            if (!usedVertices[i] && delete) {
                     model.vertices.remove(i);
-                    System.out.println("Свободная вершина удалилась");
-                }
-                else continue;
             }
         }
         ObjWriter.write("3DModels/Faceform/Test.obj", model);

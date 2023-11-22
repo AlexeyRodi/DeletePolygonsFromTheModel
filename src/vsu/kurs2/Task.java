e56c16a9ba4a441b7e31b08de470aeec3355a796
aleksey <kolohok@yandex.ru>
2023-11-22 18:26:47 +0300

    Added vertex index update

--- src/vsu/kurs2/Task.java
+++ src/vsu/kurs2/Task.java
@@ -5,26 +5,39 @@ import vsu.kurs2.model.Polygon;
 import vsu.kurs2.objwriter.ObjWriter;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Task {
     public static void deletePolygons(Model model, int k, boolean delete) {
 
+
         model.polygons.remove(k);
+
         boolean[] usedVertices = new boolean[model.vertices.size()];
         for (Polygon polygon : model.polygons) {
             ArrayList<Integer> vertexIndices = polygon.getVertexIndices();
-            for (int vertexIndex : vertexIndices) {
+            for (int i = 0; i < vertexIndices.size(); i++) {
+                int vertexIndex = vertexIndices.get(i);
                 if (vertexIndex >= 0 && vertexIndex < model.vertices.size()) {
                     usedVertices[vertexIndex] = true;
                 }
             }
         }
-        for (int i = usedVertices.length - 1; i >= 0; i--) {
-            if (!usedVertices[i] && delete) {
+        if (delete) {
+            for (int i = usedVertices.length - 1; i >= 0; i--) {
+                if (!usedVertices[i]) {
                     model.vertices.remove(i);
+                    for (Polygon polygon : model.polygons) {
+                        for (int index = 0; index < polygon.getVertexIndices().size(); index++) {
+                            if (polygon.vertexIndices.get(index) >= i) {
+                                polygon.vertexIndices.set(index, polygon.vertexIndices.get(index - 1));
+                            }
+                        }
+                    }
+                }
             }
+
+            ObjWriter.write("3DModels/Faceform/Test.obj", model);
         }
-        ObjWriter.write("3DModels/Faceform/Test.obj", model);
+
     }
 }
